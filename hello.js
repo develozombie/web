a58dde38ae35256d44cf0d944363f07b6d53d05c
Jose Yapur <yapur@outlook.com>
2019-12-26 16:39:14 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
     });
 });
 
-/* $(document).ready(function() {
+$(document).ready(function() {
     $.ajax({
-        url: "http://52.141.219.120/products/api/products",
-        headers: { 'Ocp-Apim-Subscription-Key': '9db32e4fed844af0aac4537ed2f784c8;product=unlimited' }
+        url: "http://52.154.167.44/products/api/products",
+        headers: { 'Ocp-Apim-Subscription-Key': '4f6bce226bea4eaf9bf6f2ecb2bb1916;product=unlimited' }
     }).then(function(data) {
        $('.productos').append(data[Math.floor(Math.random()*3)]);
     });
-}); */
+}); 
